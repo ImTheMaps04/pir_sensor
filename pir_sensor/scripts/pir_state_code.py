c93d5feeb15e45d624751c31829e6a5809508431
ImTheMaps04 <120549972+ImTheMaps04@users.noreply.github.com>
2022-12-14 14:49:43 +0800

    Add files via upload

---
+++ pir_sensor/scripts/pir_state_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_PIR = 7
+
+GPIO.setup(GPIO_PIR, GPIO.IN)
+
+try: 
+	while True: 
+
+	currentstate = GPIO.input(GPIO_PIR)
+	print (currentstate)
+
+except KyeboardInterrupt:
+	print("End")
+
+	
